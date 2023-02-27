c0bac70d8126ed6c3e421f3398ab4be011e9d79f
BlossBB <macma.cube1@gmail.com>
2023-02-27 09:34:14 -0600

    Frm Size

--- hc/FrmPaciente.Designer.cs
+++ hc/FrmPaciente.Designer.cs
@@ -66,12 +66,14 @@
             // 
             // tbcPaciente
             // 
+            this.tbcPaciente.Alignment = System.Windows.Forms.TabAlignment.Left;
             this.tbcPaciente.Controls.Add(this.tbpDatosPersonales);
             this.tbcPaciente.Controls.Add(this.tbpDomicilio);
             this.tbcPaciente.Location = new System.Drawing.Point(13, 13);
+            this.tbcPaciente.Multiline = true;
             this.tbcPaciente.Name = "tbcPaciente";
             this.tbcPaciente.SelectedIndex = 0;
-            this.tbcPaciente.Size = new System.Drawing.Size(710, 386);
+            this.tbcPaciente.Size = new System.Drawing.Size(812, 435);
             this.tbcPaciente.TabIndex = 0;
             // 
             // tbpDatosPersonales
@@ -96,10 +98,10 @@
             this.tbpDatosPersonales.Controls.Add(this.lblNumExpediente);
             this.tbpDatosPersonales.Controls.Add(this.txtId);
             this.tbpDatosPersonales.Controls.Add(this.lblId);
-            this.tbpDatosPersonales.Location = new System.Drawing.Point(4, 25);
+            this.tbpDatosPersonales.Location = new System.Drawing.Point(25, 4);
             this.tbpDatosPersonales.Name = "tbpDatosPersonales";
             this.tbpDatosPersonales.Padding = new System.Windows.Forms.Padding(3);
-            this.tbpDatosPersonales.Size = new System.Drawing.Size(702, 357);
+            this.tbpDatosPersonales.Size = new System.Drawing.Size(783, 427);
             this.tbpDatosPersonales.TabIndex = 0;
             this.tbpDatosPersonales.Text = "Datos Personales";
             this.tbpDatosPersonales.UseVisualStyleBackColor = true;
@@ -146,10 +148,10 @@
             this.tbpDomicilio.Controls.Add(this.lblNumeroInterior);
             this.tbpDomicilio.Controls.Add(this.txtCalle);
             this.tbpDomicilio.Controls.Add(this.lblCalle);
-            this.tbpDomicilio.Location = new System.Drawing.Point(4, 25);
+            this.tbpDomicilio.Location = new System.Drawing.Point(25, 4);
             this.tbpDomicilio.Name = "tbpDomicilio";
             this.tbpDomicilio.Padding = new System.Windows.Forms.Padding(3);
-            this.tbpDomicilio.Size = new System.Drawing.Size(702, 357);
+            this.tbpDomicilio.Size = new System.Drawing.Size(783, 427);
             this.tbpDomicilio.TabIndex = 1;
             this.tbpDomicilio.Text = "Domicilio";
             this.tbpDomicilio.UseVisualStyleBackColor = true;
@@ -354,7 +356,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(968, 568);
             this.Controls.Add(this.tbcPaciente);
             this.Name = "FrmPaciente";
             this.Text = "FrmPaciente";
